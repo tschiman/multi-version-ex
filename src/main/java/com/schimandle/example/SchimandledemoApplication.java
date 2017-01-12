c5ecf8599d6010818e36b4641cffdf1ab2bbeb1c
tschiman <tschiman@gmail.com>
2017-01-11 22:02:34 -0700

    first commit

---
+++ src/main/java/com/schimandle/example/SchimandledemoApplication.java
@@ -0,0 +1,12 @@
+package com.schimandle.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchimandledemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchimandledemoApplication.class, args);
+	}
+}
